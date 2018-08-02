83c15f65d29d07909b02bf9604ce3e652458331e
yume_yu <yu.matchang@gmail.com>
2018-08-03 02:21:03 +0900

    javadoc用コメントを追記。もうやだ

--- src/main/java/main/Window.java
+++ src/main/java/main/Window.java
@@ -1,3 +1,7 @@
+/**
+ * Mainメソッドに呼ばれるウィンドウを統括するメソッド
+ * @author Yu Matsuda
+ */
 package main;
 
 import javax.swing.*;
@@ -10,14 +14,38 @@ import java.util.Date;
 
 public class Window extends JFrame {
 
+    /**
+     * ディスプレイの大きさを取得し格納する変数
+     */
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    /**
+     * JPanelを格納するコンテナ
+     */
     private Container contentPane = getContentPane();
+    /**
+     * Clockペインを代入する変数
+     */
     private JPanel clock;
+    /**
+     * Calendarペインを代入する変数
+     */
     private JPanel calender;
+    /**
+     * Forecastペインを代入する変数
+     */
     private JPanel forecast;
+    /**
+     * ウィンドウの幅
+     */
     private int width = 300;
+    /**
+     * ウィンドウの高さ
+     */
     private int height = 500;
 
+    /**
+     * フレームを既定の大きさで作成します
+     */
     public Window(){
         setTitle("MyApplication");
         setBounds(screenSize.width - width,0,width,height);
@@ -25,16 +53,25 @@ public class Window extends JFrame {
         setResizable(false);
     }
 
+    /**
+     * contentPaneにClockペインを追加します
+     */
     public void addClock(){
         clock = new Clock(width,height / 20);
         contentPane.add(clock, BorderLayout.SOUTH);
     }
 
+    /**
+     * contentPaneにCalendarペインを追加します
+     */
     public void addCalendar(){
         calender = new Calendar(width, height / 2);
         contentPane.add(calender,BorderLayout.NORTH);
     }
 
+    /**
+     * contentPaneにForecastペインを追加します
+     */
     public void addForecast(){
         forecast = new Forecast(width, (height / 2) - (height / 20));
         contentPane.add(forecast,BorderLayout.CENTER);
